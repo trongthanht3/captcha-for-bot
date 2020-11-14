d7ddce4a445dda104e2a9ac0969446d3ac247c99
Hoàng <hoangnh5901@outlook.com>
2020-11-15 00:01:02 +0700

    add gitkeep

--- make_dir.py
+++ make_dir.py
@@ -1,7 +1,21 @@
 import os
 import string
+import subprocess
 
-for c in string.ascii_lowercase:
-    os.makedirs('train_data/' + c.upper())
-for i in range(10):
-    os.makedirs('train_data/' + str(i))
+
+def make_dir():
+    for c in string.ascii_lowercase:
+        os.makedirs('train_data/' + c.upper())
+        subprocess.check_output('touch train_data/' + c.upper() + "/.gitkeep", shell=True)
+    for i in range(10):
+        os.makedirs('train_data/' + str(i))
+
+
+def add_git_keep():
+    for c in string.ascii_lowercase:
+        subprocess.check_output('touch train_data/' + c.upper() + "/.gitkeep", shell=True)
+    for i in range(10):
+        subprocess.check_output('touch train_data/' + str(i) + "/.gitkeep", shell=True)
+
+
+add_git_keep()
