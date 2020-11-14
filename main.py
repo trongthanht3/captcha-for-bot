8c58f9260fa6a126198e87d062b8897e24d9e601
trongthanht3 <trongthanht3@gmail.com>
2020-11-15 00:45:14 +0700

    change kernel_denoise to (2x2)

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import cv2
 import numpy as np
 
-img = cv2.imread('124317847_569194560566206_7089234066176291289_n.jpg', 0)
+img = cv2.imread('goc.jpg', 0)
 x0, y0 = img.shape
 
-img_ori = cv2.imread('124317847_569194560566206_7089234066176291289_n.jpg')
+img_ori = cv2.imread('goc.jpg')
 img = cv2.resize(img, (y0 * 2, x0 * 2))
 img_ori = cv2.resize(img_ori, (y0 * 2, x0 * 2))
 
@@ -13,6 +13,7 @@ arr1 = np.ones(img.shape, dtype=np.uint8) * 255
 print(arr.shape)
 print(img.shape)
 img_not = cv2.bitwise_not(img, arr)
+
 # img_xor = cv2.bitwise_and(img, arr1)
 # img = cv2.resize(img,(320,120))
 # img_ori = cv2.resize(img_ori,(320,120))
@@ -28,16 +29,17 @@ img_not = cv2.bitwise_not(img, arr)
 kernel = np.array([[0, -5, 0],
                    [-5, 20, -5],
                    [0, -5, 0]])
-
+kernel_denoise = np.ones((2,2), np.uint8)
+kernel_denoise2 = np.ones((1,1), np.uint8)
 img_not = cv2.filter2D(img_not, -1, kernel)
 
-kernel_denoise = np.ones((3, 3), np.uint8)
 # # erosion = cv.erode(img,kernel,iterations = 1)
 #
-# img_no = cv2.morphologyEx(img_not, cv2.MORPH_OPEN, kernel_denoise)
 # cv2.medianBlur(img_not, 3, img_not)
 
 cv2.dilate(img_not, kernel_denoise, img_not, iterations=1)
+# img_not = cv2.morphologyEx(img_not, cv2.MORPH_OPEN, kernel_denoise2)
+# img_not = cv2.medianBlur(img_not,3) #3
 cv2.threshold(img_not, 127, 255, cv2.THRESH_BINARY_INV, img_not)
 
 xmin = -1;
@@ -75,11 +77,14 @@ ymax = int(x0 * 0.85)
 cv2.imshow('1', img_not[ymin:ymax, xmin:xmax])
 img_new = img_not[ymin:ymax, xmin:xmax]
 y, x = img_new.shape
-cut =6
-dis = int((xmax - xmin) / cut)
-# for i in range(cut):
-    # cv2.imshow(str(i) + ".jpg", img_new[:, dis * i:dis * (i + 1)])
-# cv2.imshow('2', img_no)
+
+# cut ảnh với số mẫu 4-8
+for cut in range(4,9):
+    dis = int((xmax - xmin) / cut)
+    for i in range(cut):
+        cv2.imshow('cache/' + str(cut) + '_' + str(i) + ".jpg", img_new[:, dis * i:dis * (i + 1)])
+        cv2.imwrite('cache/' + str(cut) + '_' + str(i) + ".jpg", img_new[:, dis * i:dis * (i + 1)])
+cv2.imshow('2', img_not)
 
 # cv2.imwrite('anh.jpg', img_not)
 
